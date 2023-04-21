4a23abdba3eb7f07fed94f889cc719e602b73340
leehaakjoo <l--h--j--@naver.com>
2023-04-21 17:08:10 +0900

    user login 후 받은 token으로 유저 정보 조회 구현

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -1,8 +1,14 @@
 import { baseApi } from "./index";
 
 export const userApi = {
-  login: (Data: any) => baseApi.post(`/user/login`, Data),
-  getUser: (Data: any) => baseApi.post(`/user/getuser`, Data),
+  login: (Data: any) => baseApi.post(`/auth/login`, Data),
+
+  getUser: (token: string) =>
+    baseApi.get(`/auth`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }),
   setting: (Data: any) => baseApi.post(`/user/setting`, Data),
   register: (Data: any) => {
     const res = baseApi.post("/user/register", Data);
--- src/views/login/LoginContainer.tsx
+++ src/views/login/LoginContainer.tsx
@@ -1,6 +1,4 @@
-import ViewCard from "@/context/viewcard/ViewCard";
-
-import { useContext } from "react";
+import { useContext, useRef, useState } from "react";
 import { Button, TextField } from "@material-ui/core";
 
 import { ViewCardContext } from "@/context";
@@ -8,13 +6,48 @@ import useUserForm from "@/hooks/user/User";
 
 import "./login.scss";
 import { userApi } from "@/apis/user";
+import { HttpStatusCode } from "axios";
 
 const Login = () => {
   const ViewCard = useContext(ViewCardContext);
   const { form, handleFormChange } = useUserForm();
 
+  const [notValidated, setNotValidated] = useState<string>("");
+
+  const userIdRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const getUser = async (token: string) => {
+    const res = await userApi.getUser(token);
+    console.log("res", res);
+  };
   const handleSubmit = async () => {
     const res = await userApi.login(form);
+    if (!res.data) alert("알 수 없는 오류입니다.");
+    switch (res.data.statusCode) {
+      case HttpStatusCode.Ok:
+        alert("로그인 성공");
+        console.log("res.data.data", res.data.data);
+        await getUser(res.data.data.access_token);
+        break;
+      case HttpStatusCode.NotFound:
+        alert("유저를 찾을 수 없습니다");
+        userIdRef.current?.focus();
+        setNotValidated("userId");
+        break;
+      case HttpStatusCode.Unauthorized:
+        alert("비밀번호 오류");
+        passwordRef.current?.focus();
+        setNotValidated("password");
+        break;
+      default:
+        alert("알 수 없는 오류 ");
+    }
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.name === notValidated) setNotValidated("");
+    handleFormChange(e);
   };
 
   return (
@@ -22,20 +55,26 @@ const Login = () => {
       <div className="login-container">
         <span className="login-title">LOGIN</span>
         <TextField
-          name="id"
+          name="userId"
           value={form.userId}
           variant="outlined"
           label="ID"
           margin="normal"
-          onChange={handleFormChange}
+          autoFocus
+          error={notValidated === "userId"}
+          onChange={handleInputChange}
+          inputRef={userIdRef}
         />
         <TextField
           name="password"
+          type="password"
           value={form.password}
           variant="outlined"
           label="Password"
           margin="normal"
-          onChange={handleFormChange}
+          error={notValidated === "password"}
+          onChange={handleInputChange}
+          inputRef={passwordRef}
         />
         <Button
           size={"large"}
